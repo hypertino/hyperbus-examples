346a5e2a35f4d65eabeb72089ccda6fd15c389ef
Maga Abdurakhmanov <maqdev@gmail.com>
2017-07-04 22:58:25 +0200

    + working example

--- example-service/src/main/scala/com/hypertino/exampleservice/ExampleService.scala
+++ example-service/src/main/scala/com/hypertino/exampleservice/ExampleService.scala
@@ -1,12 +1,13 @@
 package com.hypertino.exampleservice
 
-import com.hypertino.exampleservice.api.{TodoItem, TodoList, TodoListGet}
+import com.hypertino.exampleservice.api._
 import com.hypertino.hyperbus.Hyperbus
-import com.hypertino.hyperbus.model.Ok
+import com.hypertino.hyperbus.model.{Accepted, Created, EmptyBody, ErrorBody, NoContent, NotFound, Ok}
 import com.hypertino.service.control.api.{Console, Service}
 import monix.execution.Ack.Continue
 import monix.execution.Scheduler
 import scaldi.{Injectable, Injector}
+
 import scala.concurrent.duration._
 import scala.util.Success
 
@@ -16,21 +17,52 @@ class ExampleService (console: Console, implicit val injector: Injector) extends
   implicit val scheduler = inject[Scheduler]
   val hyperbus = inject[Hyperbus]
 
-  val handlers = Seq {
+  var todoList = TodoList("This is my todo list", List(
+    TodoItem("1", "Make exercise"),
+    TodoItem("2", "Buy milk")
+  ))
+  var counter = 2
+
+  val handlers = Seq (
     hyperbus.commands[TodoListGet].subscribe { implicit command ⇒
-
-      val result = TodoList("This is my todo list", List(
-        TodoItem("1", "Make exercise"),
-        TodoItem("2", "Buy milk")
-      ))
-
       command.reply(Success(
-        Ok(result)
+        Ok(todoList)
       ))
+      Continue
+    },
 
+    hyperbus.commands[TodoListPost].subscribe { implicit command ⇒
+      counter += 1
+      todoList = todoList.copy(
+        items = todoList.items :+ TodoItem(counter.toString, command.request.body.title)
+      )
+      command.reply(Success(
+        Created(EmptyBody)
+      ))
+      Continue
+    },
+
+    hyperbus.commands[TodoListItemGet].subscribe { implicit command ⇒
+      command.reply(
+        todoList.items.find(_.id == command.request.id) match {
+          case Some(item) ⇒ Success(Ok(item))
+          case None ⇒ Success(NotFound(ErrorBody("item-not-found", Some(s"Item ${command.request.id} is not found"))))
+        }
+      )
+      Continue
+    },
+
+    hyperbus.commands[TodoListItemDelete].subscribe { implicit command ⇒
+      counter += 1
+      todoList = todoList.copy(
+        items = todoList.items.filterNot(_.id == command.request.id)
+      )
+      command.reply(Success(
+        Accepted(EmptyBody)
+      ))
       Continue
     }
-  }
+  )
 
   def stopService(controlBreak: Boolean): Unit = {
     handlers.foreach(_.cancel())
